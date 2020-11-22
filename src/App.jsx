3ff3c9832e17bed67258af11828cde5c05fec301
faeze1379 <74100070+faeze1379@users.noreply.github.com>
2020-11-22 10:36:50 -0800

    u app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
       this.state = {
          arr:[
             {
-               name:"faeze",
+               name:"faeze firozi",
                id:"1"
                },
                {
